396e81ee8792b605540e6c61fa35c741d4fcf0b4
Nikolay <palmer@bk.ru>
2023-07-05 00:15:09 +0300

    working with inputs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function useInput(initialValue) {
+
+        const [value, setValue] = useState(initialValue);
+
+        return [{value, onChange: (e) => setValue(e.target.value)}, () => setValue(initialValue)];
+    }
+
+
+    const [titleProps, resetTitle] = useInput("");
+    const [colorProps, resetColor] = useInput("#999999");
+
+    const submit = (e) => {
+        e.preventDefault(); //чтобы не перезагрузилась страница
+        alert(`${titleProps.value}, ${colorProps.value}`);
+        resetTitle();
+        resetColor();
+
+    }
+
+    return (
+
+
+        <div className="App">
+            <form onSubmit={submit}>
+                <input
+                    /*вместе значений почему то сделали там, по другому не работает при использование не хук нужно будет опредлить value и значение onChange */
+
+                    // defaultValue={titleProps.value.toString()}
+                    // onChange={(event) => (resetTitle())} так сделать нельзя
+                    {...titleProps} //так как у нас отдается и value и onChange
+                    type="text" placeholder="type color"></input>
+
+                <input
+                    {...colorProps}
+                    type="color"></input>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
