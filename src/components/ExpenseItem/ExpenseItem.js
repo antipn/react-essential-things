f77ace5fbd093f7ccbdbae813bdc16e6a72ac903
Nikolay <palmer@bk.ru>
2024-04-23 12:02:34 +0300

    38 step

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,10 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-
+    const year = props.date.getFullYear();
+    //const year = props.date.toLocaleString('ru-RU', {year: 'numeric'});
+    const month = props.date.toLocaleString('ru-RU', {month: 'long'}).toUpperCase();
+    const day = props.date.toLocaleString('ru-RU', {day: '2-digit'});
 
     return (<div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
             </div>
